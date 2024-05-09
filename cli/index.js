95ce3652a89a7ad864d3fd20f2a9b0e487d72e54
Samreen <samreen.khatoon@cloudesign.com>
2024-05-09 12:55:01 +0530

    cli folder update

--- cli/index.js
+++ cli/index.js
@@ -8,22 +8,10 @@ const fs = require('fs');
 const scriptDir = __dirname;
 
 console.log(scriptDir)
-
-// Assuming the repository is installed in the `node_modules` directory
-const repoDir = path.join(scriptDir, '..', '..');
+const repoDir = path.join(scriptDir, '..', '..', '..');
 
 console.log(repoDir)
 
-// Assuming "src" is the source folder of the repository
 const srcDir = path.join(repoDir, 'src');
 
 console.log(srcDir)
-
-// Check if the "src" directory exists within the repository
-fs.stat(srcDir, (err, stats) => {
-    if (err || !stats.isDirectory()) {
-        console.log('Source directory not found for the installed repository.');
-    } else {
-        console.log(`Source directory of the installed repository is located at: ${srcDir}`);
-    }
-});
