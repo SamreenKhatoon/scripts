d21c86c90c151c6b5d1b27ee56251d8964b8828c
Samreen <samreen.khatoon@cloudesign.com>
2024-05-09 16:38:19 +0530

    changes

--- cli/index.js
+++ cli/index.js
@@ -1,17 +1,9 @@
 #!/usr/bin/env node
 
-// console.log("Script called")
-
-// const path = require('path');
-
-// const repoDir = path.join(__dirname, '..', '..', '..');
-
-// console.log(repoDir)
-
-// const srcDir = path.join(repoDir, 'src');
-
-// console.log(srcDir)
-
 const path = require('path');
-console.log(__dirname);
-console.log(__filename);
+// console.log(__dirname);
+
+let index = __dirname.indexOf('/node_modules');
+
+let result = __dirname.substring(0, index + __dirname.length);
+console.log(result);
