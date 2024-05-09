a413451de48474a2bf474c4f49102ccdacc71215
Samreen <samreen.khatoon@cloudesign.com>
2024-05-09 16:41:47 +0530

    changes to script path

--- cli/index.js
+++ cli/index.js
@@ -1,9 +1,10 @@
 #!/usr/bin/env node
 
 const path = require('path');
-// console.log(__dirname);
+console.log(__dirname.length);
 
 let index = __dirname.indexOf('node_modules');
+console.log(index)
 
 let result = __dirname.substring(0, index + __dirname.length);
 console.log(result);
